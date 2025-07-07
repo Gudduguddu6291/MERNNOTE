fd533c662ab0c6923dbd1be9a9dcf65904081dda
Gudduguddu6291 <155060381+Gudduguddu6291@users.noreply.github.com>
2025-07-07 20:03:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,13 +19,13 @@ app.get("/", (req, res) => {
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "https://mernnote-frontend.onrender.com", // or your frontend URL
+  origin: "https://mernnote-uwjf.onrender.com", // or your frontend URL
   credentials: true
 }));
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://mernnote-frontend.onrender.com",
+    origin: "https://mernnote-uwjf.onrender.com",
     credentials: true
   }
 });
